1e300736dd9c0d88f4774cccb0485fba976573b2
Yaroslav <sachayara@gmail.com>
2020-08-16 14:03:12 +0300

    k

--- project_bouncy/js/main.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('#menu-togle');
-const mobileNavContainer = document.querySelector('#mobile-nav');
-
-menuToggle.onclick = function(){
-    menuToggle.classList.toggle('burger-icon-active');
-    mobileNavContainer.classList.toggle('mobile-nav--active');
-}
